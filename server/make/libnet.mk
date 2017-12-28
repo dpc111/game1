644a2e425ffeaa3013af56a7588746d8049750ba
dpc111 <1539309743@.com>
2017-12-28 17:40:06 +0800

    l

--- server/make/libnet.mk
+++ server/make/libnet.mk
@@ -2,8 +2,8 @@ include core.include
 #链接器参数,  连接时搜索指定的函数库LDFLAGS。,引入需要的库-lLDFLAGS    指定编译的时候使用的库. 连接库文件开关。例如-lugl，则是把程序同libugl.a文件进行连接。
 #连接时使用
 #-lclntsh -lnsl -lpthread -Wl,-Bdynamic -lgcc_s    ,同时有动态库和静态库时默认使用动态库，   -Wl,-Bdynamic 指定和动态库相连， -Wl,-Bstatic 指定和静态库相连
-CLDFLAGS := -lm -lrt -lnsl -lpthread -lbz2  -Wl,-Bdynamic  -lgcc_s -lcommon
-CXXLDFLAGS := -lm -lrt -lnsl -lpthread -lbz2 -Bdynamic -lcommon
+CLDFLAGS := -lm -lrt -lnsl -lpthread  -Wl -Bdynamic -lgcc_s -lcommon
+CXXLDFLAGS := -lm -lrt -lnsl -lpthread -Bdynamic -lcommon
 MODULEFLAGS := -fPIC -shared -lpthread -o
 
 ##########################################################################################

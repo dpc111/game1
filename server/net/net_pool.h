b443e42b604220feb916ec6a0eb04d857bf45571
dpc111 <1539309743@qq.com>
2017-12-31 00:36:46 +0800

    l

--- server/net/net_pool.h
+++ server/net/net_pool.h
@@ -9,7 +9,7 @@ typedef obj_pool_t<input_chunk_t> input_pool_t;
 
 static tls_t tls_input_pool;
 
-static input_pool_t *input_chunk_alloc() {
+static input_chunk_t *input_chunk_alloc() {
 	input_pool_t *pool = (input_pool_t *)tls_input_pool.get();
 	if (pool == NULL) {
 		pool = new input_pool_t;
@@ -19,7 +19,7 @@ static input_pool_t *input_chunk_alloc() {
 }
 
 static void input_chunk_free(input_chunk_t *chunk) {
-	input_pool_t *pool = tls_input_pool.get();
+	input_pool_t *pool = (input_pool_t *)tls_input_pool.get();
 	assert(pool);
 	pool->free(chunk);
 }
@@ -29,8 +29,8 @@ typedef obj_pool_t<output_chunk_t> output_pool_t;
 
 static tls_t tls_output_pool;
 
-static output_pool_t *output_chunk_alloc() {
-	output_pool_t *pool = tls_output_pool.get();
+static output_chunk_t *output_chunk_alloc() {
+	output_pool_t *pool = (output_pool_t *)tls_output_pool.get();
 	if (pool == NULL) {
 		pool = new output_pool_t;
 		tls_output_pool.set(pool);
@@ -39,7 +39,7 @@ static output_pool_t *output_chunk_alloc() {
 }
 
 static void output_chunk_free(output_chunk_t *chunk) {
-	output_pool_t *pool = tls_output_pool.get();
+	output_pool_t *pool = (output_pool_t *)tls_output_pool.get();
 	assert(pool);
 	pool->free(chunk);
 }
@@ -48,7 +48,7 @@ static void output_chunk_free(output_chunk_t *chunk) {
 static tls_t tls_read_fd_buff;
 
 static input_queue_t& assign_read_fd_buff() {
-	input_queue_t *read_fd_buff = tls_read_fd_buff.get();
+	input_queue_t *read_fd_buff = (input_queue_t *)tls_read_fd_buff.get();
 	if (read_fd_buff == NULL) {
 		read_fd_buff = new input_queue_t;
 		tls_read_fd_buff.set(read_fd_buff);
@@ -66,7 +66,7 @@ typedef obj_pool_t<tcp_connection_t> connection_pool_t;
 static tls_t tls_connection_pool;
 
 static tcp_connection_t *connection_alloc() {
-	tcp_connection_t *pool = tls_connection_pool.get();
+	tcp_connection_t *pool = (tcp_connection_t *)tls_connection_pool.get();
 	if (pool == NULL) {
 		pool = new tcp_connection_t;
 		tls_connection_pool.set(pool);
@@ -75,7 +75,7 @@ static tcp_connection_t *connection_alloc() {
 }
 
 static void connection_free(tcp_connection_t *conn) {
-	tls_connection_pool *pool = tls_connection_pool.get();
+	tls_connection_pool *pool = (tls_connection_pool *)tls_connection_pool.get();
 	assert(pool);
 	pool->free(conn);
 }

44c86add961db50c0322c6ee792afdd412c9c96a
dpc111 <1539309743@qq.com>
2018-01-01 12:05:46 +0800

    ..

--- server/make/libnet.mk
+++ server/make/libnet.mk
@@ -4,7 +4,7 @@ include core.include
 #连接时使用
 #-lclntsh -lnsl -lpthread -Wl,-Bdynamic -lgcc_s    ,同时有动态库和静态库时默认使用动态库，   -Wl,-Bdynamic 指定和动态库相连， -Wl,-Bstatic 指定和静态库相连
 CLDFLAGS := -lm -lrt -lnsl -lpthread -Wl,-Bdynamic
-CXXLDFLAGS := -lm -lrt -lnsl -lpthread -Wl,-Bdynamic -levent -lprotobuf -Bstatic -lmessage -Bynamic -lcommon -lgcc_s
+CXXLDFLAGS := -lm -lrt -lnsl -lpthread -Wl,-Bdynamic -levent -lprotobuf -Bynamic -lcommon -lgcc_s
 MODULEFLAGS := -fPIC -shared -o
 
 ##########################################################################################

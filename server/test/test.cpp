c82f15841ea318536875f7c2bcc5317239b9a46a
dpc111 <1539309743@qq.com>
2018-01-07 00:50:20 +0800

    l

--- server/test/test.cpp
+++ server/test/test.cpp
@@ -5,11 +5,12 @@
 #include "tcp_network.h"
 
 #include "timer.h"
+#include "timestamp.h"
 #include "server.h"
 
 class test_timer : public timer_handler_t {
 	virtual void handle_timeout(timer_handle_t handle, void *user) {
-		printf("aaaaaahhshsdfdfs\n");
+		printf("aaaaaahhshsdfdfs%ld\n", getms());
 	}
 };
 
@@ -18,11 +19,10 @@ int main() {
 	// tcp_network_t network("0.0.0.0", 7768);
 	// network.start();
 
-
 	server_t server("0.0.0.0", 7768);
 	server.start();
 	test_timer timer;
-	server.register_timer(&timer, NULL, 1000, 2000);
+	server.register_timer(&timer, NULL, 100000, 200000);
 	server.process();
 
 	printf("llllllllllsdssd\n");

aaa551ca2220c5c7bc9904504e5a4d9f2bb15088
dpc111 <1539309743@qq.com>
2018-01-06 23:51:19 +0800

    l

--- server/make/test.mk
+++ server/make/test.mk
@@ -4,7 +4,7 @@ include core.include
 #连接时使用
 #-lclntsh -lnsl -lpthread -Wl,-Bdynamic -lgcc_s    ,同时有动态库和静态库时默认使用动态库，   -Wl,-Bdynamic 指定和动态库相连， -Wl,-Bstatic 指定和静态库相连
 CLDFLAGS := -lm -lrt -lnsl -lpthread -Wl,-Bdynamic
-CXXLDFLAGS := -lm -lrt -lnsl -lpthread -Wl,-Bdynamic -lnet -lcommon -levent -lprotobuf -lgcc_s
+CXXLDFLAGS := -lm -lrt -lnsl -lpthread -Wl,-Bdynamic -lserver -lnet -lcommon -levent -lprotobuf -lgcc_s
 MODULEFLAGS := -fPIC -g3 -o
 
 ##########################################################################################

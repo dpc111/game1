eb93ee6a845d143ad62cb77e780ea636def393c3
dpc111 <1539309743@qq.com>
2018-01-19 15:15:49 +0800

    ..

--- server/test/test.cpp
+++ server/test/test.cpp
@@ -33,7 +33,7 @@ int main() {
 	server.get_lua_frame()->run_script();
 	server.start();
 	test_timer timer;
-	server.register_timer(&timer, NULL, 10000, 300);
+	server.register_timer(&timer, NULL, 10000, 30);
 
 	// test
 	 conn = server.connect_to("127.0.0.1", 7768);

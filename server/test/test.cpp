2e78bbdadfcd62720296e72a0c91ac3930230ba9
dpc111 <1539309743@qq.com>
2018-01-23 16:07:18 +0800

    ..

--- server/test/test.cpp
+++ server/test/test.cpp
@@ -1,3 +1,52 @@
+// #include "stdio.h"
+// #include "log.h"
+// #include "thread.h"
+// #include "net_address.h"
+// #include "tcp_network.h"
+// #include "tcp_connection.h"
+
+// #include "timer.h"
+// #include "timestamp.h"
+// #include "server.h"
+// #include "lua_frame.h"
+// #include "message/messages.h"
+
+// //server_t server("0.0.0.0", 7768);
+// server_t server("0.0.0.0", 7769);
+// tcp_connection_t *conn;
+
+// class test_timer : public timer_handler_t {
+// public:
+// 	virtual void handle_timeout(timer_handle_t handle, void *user) {
+// 		int res;
+// 		bool ok = server.get_lua_frame()->call_func("test_add", "ii:i", 1, 2, &res);
+// 		for (int i = 1; i < 2; i++) 
+// 			server.send_func(conn, "msg_func", "iis", 1, 8, "dpc");
+// 		battle::s2c_join msg;
+// 		msg.set_uid(11111);
+// 		msg.set_name("dpc ddy");
+// 		msg.set_icon("icon url");
+// 		server.send(conn, msg);
+// 		assert(ok);
+// 	}
+
+// };
+
+// int main() {
+// 	server.get_lua_frame()->load_script("test.lua");
+// 	server.get_lua_frame()->run_script();
+// 	server.start();
+// 	test_timer timer;
+// 	server.register_timer(&timer, NULL, 10000, 2000);
+
+// 	conn = server.connect_to("127.0.0.1", 7768);
+
+// 	server.process();
+
+// 	return 1;
+// }
+
+
 #include "stdio.h"
 #include "log.h"
 #include "thread.h"
@@ -11,21 +60,14 @@
 #include "lua_frame.h"
 #include "message/messages.h"
 
-//server_t server("0.0.0.0", 7768);
-server_t server("0.0.0.0", 7769);
+server_t server("0.0.0.0", 7768);
 tcp_connection_t *conn;
 
 class test_timer : public timer_handler_t {
-public:
 	virtual void handle_timeout(timer_handle_t handle, void *user) {
 		int res;
 		bool ok = server.get_lua_frame()->call_func("test_add", "ii:i", 1, 2, &res);
-	//	printf(".....%d\n", res);
-	//	ERROR("");
-		// test
-		for (int i = 1; i < 1000; i++) 
-			server.send_func(conn, "msg_func", "iis", 1, 8, "dpc");
-	//	ERROR("");
+		printf(".....%d\n", res);
 		assert(ok);
 	}
 
@@ -34,12 +76,6 @@ public:
 		ERROR("%s", msg.name());
 		ERROR("%s", msg.icon());
 	}
-
-	void register_msg(server_t *server) {
-		server->register_net_message<battle::s2c_join>(std::tr1::bind(&test_timer::test_msg, this, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
-		// server->get_network()->get_msg_dispatch()->register_net_message<battle::s2c_join>(std::tr1::bind(&test_timer::test_msg, this, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
-	}
-
 };
 
 int main() {
@@ -47,51 +83,11 @@ int main() {
 	server.get_lua_frame()->run_script();
 	server.start();
 	test_timer timer;
-	server.register_timer(&timer, NULL, 10000, 2000);
-	timer.register_msg(&server);
-
-	// test
-	 conn = server.connect_to("127.0.0.1", 7768);
+	server.register_timer(&timer, NULL, 10000, 10000);
+	server.register_net_message<battle::s2c_join>(std::tr1::bind(&test_timer::test_msg, &timer, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
 
 	server.process();
 
 	return 1;
 }
-
-
-// #include "stdio.h"
-// #include "log.h"
-// #include "thread.h"
-// #include "net_address.h"
-// #include "tcp_network.h"
-// #include "tcp_connection.h"
-
-// #include "timer.h"
-// #include "timestamp.h"
-// #include "server.h"
-// #include "lua_frame.h"
-
-// server_t server("0.0.0.0", 7768);
-// tcp_connection_t *conn;
-
-// class test_timer : public timer_handler_t {
-// 	virtual void handle_timeout(timer_handle_t handle, void *user) {
-// 		int res;
-// 		bool ok = server.get_lua_frame()->call_func("test_add", "ii:i", 1, 2, &res);
-// 		printf(".....%d\n", res);
-// 		assert(ok);
-// 	}
-// };
-
-// int main() {
-// 	server.get_lua_frame()->load_script("test.lua");
-// 	server.get_lua_frame()->run_script();
-// 	server.start();
-// 	test_timer timer;
-// 	server.register_timer(&timer, NULL, 10000, 10000);
-
-// 	server.process();
-
-// 	return 1;
-// }
 

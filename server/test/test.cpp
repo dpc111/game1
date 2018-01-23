12d0c190ef17ebb73ec249c5bc469b55e044bb12
dpc111 <1539309743@qq.com>
2018-01-23 17:27:32 +0800

    ..

--- server/test/test.cpp
+++ server/test/test.cpp
@@ -1,51 +1,58 @@
-// #include "stdio.h"
-// #include "log.h"
-// #include "thread.h"
-// #include "net_address.h"
-// #include "tcp_network.h"
-// #include "tcp_connection.h"
+#define CLIENT
+//////////////////////////////////////////////////////////////////////////////////////////////////
+#ifdef CLIENT
 
-// #include "timer.h"
-// #include "timestamp.h"
-// #include "server.h"
-// #include "lua_frame.h"
-// #include "message/messages.h"
+#include "stdio.h"
+#include "log.h"
+#include "thread.h"
+#include "net_address.h"
+#include "tcp_network.h"
+#include "tcp_connection.h"
 
-// //server_t server("0.0.0.0", 7768);
-// server_t server("0.0.0.0", 7769);
-// tcp_connection_t *conn;
+#include "timer.h"
+#include "timestamp.h"
+#include "server.h"
+#include "lua_frame.h"
+#include "message/messages.h"
 
-// class test_timer : public timer_handler_t {
-// public:
-// 	virtual void handle_timeout(timer_handle_t handle, void *user) {
-// 		int res;
-// 		bool ok = server.get_lua_frame()->call_func("test_add", "ii:i", 1, 2, &res);
-// 		for (int i = 1; i < 2; i++) 
-// 			server.send_func(conn, "msg_func", "iis", 1, 8, "dpc");
-// 		battle::s2c_join msg;
-// 		msg.set_uid(11111);
-// 		msg.set_name("dpcddy");
-// 		msg.set_icon("iconurl");
-// 		server.send(conn, msg);
-// 		assert(ok);
-// 	}
+//server_t server("0.0.0.0", 7768);
+server_t server("0.0.0.0", 7769);
+tcp_connection_t *conn;
 
-// };
+class test_timer : public timer_handler_t {
+public:
+	virtual void handle_timeout(timer_handle_t handle, void *user) {
+		int res;
+		bool ok = server.get_lua_frame()->call_func("test_add", "ii:i", 1, 2, &res);
+		for (int i = 1; i < 2; i++) 
+			server.send_func(conn, "msg_func", "iis", 1, 8, "dpc");
+		battle::s2c_join msg;
+		msg.set_uid(11111);
+		msg.set_name("dpcddy");
+		msg.set_icon("iconurl");
+		server.send(conn, msg);
+		assert(ok);
+	}
 
-// int main() {
-// 	server.get_lua_frame()->load_script("test.lua");
-// 	server.get_lua_frame()->run_script();
-// 	server.start();
-// 	test_timer timer;
-// 	server.register_timer(&timer, NULL, 10000, 2000);
+};
 
-// 	conn = server.connect_to("127.0.0.1", 7768);
+int main() {
+	server.get_lua_frame()->load_script("test.lua");
+	server.get_lua_frame()->run_script();
+	server.start();
+	test_timer timer;
+	server.register_timer(&timer, NULL, 10000, 2000);
 
-// 	server.process();
+	conn = server.connect_to("127.0.0.1", 7768);
 
-// 	return 1;
-// }
+	server.process();
 
+	return 1;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+#else
 
 #include "stdio.h"
 #include "log.h"
@@ -92,3 +99,5 @@ int main() {
 	return 1;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+#endif

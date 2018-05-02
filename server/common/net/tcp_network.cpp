aa5ec25d9e65b8c23219407785c0a0cd6ba97791
dpc111 <1539309743@qq.com>
2018-05-02 16:58:55 +0800

    l

--- server/common/net/tcp_network.cpp
+++ server/common/net/tcp_network.cpp
@@ -125,7 +125,7 @@ void tcp_network_t::ev_listen_cb(evconnlistener *listener, evutil_socket_t fd, s
 	conn->set_events(network->get_ev_base(), ev_read_cb, ev_write_cb);
 	conn->set_state(CNT_STATE_CONNECTED);
 	network->add_connection(conn);
-	ERROR("connect fd %d addr %s", fd, conn->get_peer_addr().to_ip_port());
+	ERROR("connect fd %d addr %s", fd, conn->get_peer_addr().to_ip_port().c_str());
 }
 
 void tcp_network_t::ev_read_cb(evutil_socket_t fd, const short which, void *arg) {

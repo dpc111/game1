05069e4b689592569422c4d3f612ce85db5302a7
dpc111 <1539309743@qq.com>
2018-05-08 18:01:47 +0800

    l

--- server/battle/msg/client_msg.cpp
+++ server/battle/msg/client_msg.cpp
@@ -16,14 +16,14 @@ void client_msg_t::s2c_join(tcp_connection_t *conn, const battle::s2c_join& msg)
 }
 
 void client_msg_t::c2s_join(tcp_connection_t *conn, const battle::c2s_join& msg) {
-	int64 uid = msg.uid
-	player_t *player = get_player_mgr()->get_player();
+	int64 uid = msg.uid();
+	player_t *player = get_player_mgr()->get_player(uid);
 	if (player) {
 		return;
 	}
 	player = new player_t(uid, "dpc", 1, "null");
 	get_player_mgr()->add_player(uid, player);
-	
+
 	battle::s2c_join res;
 	res.set_uid(uid);
 	res.set_name("dpc");

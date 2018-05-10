24167c2b19bfa1de7868888a7baf8afeee8c9219
dpc111 <1539309743@qq.com>
2018-05-10 14:08:43 +0800

    l

--- server/common/config/cfg_json_mgr.cpp
+++ server/common/config/cfg_json_mgr.cpp
@@ -13,8 +13,10 @@ cfg_json_mgr_t::~cfg_json_mgr_t() {
 void cfg_json_mgr_t::load(std::string file_name) {
 	cfg_json_t *json = new cfg_json_t(this);
 	if (json->init(file_name.c_str())) {
+		ERROR("load json file: %s", file_name);
 		jsons_[file_name] = json;
 	} else {
+		ERROR("load json file: %s failed", file_name);
 		delete json;
 	}
 }

cd845b10a2714a7907469bd44b5e0813f68b1191
dpc111 <1539309743@qq.com>
2018-05-11 18:41:15 +0800

    l

--- server/common/server/server.cpp
+++ server/common/server/server.cpp
@@ -24,8 +24,8 @@ server_t::~server_t() {
 
 void server_t::init() {
 	json_mgr_->load("battle.json", "battle");
-	ERROR("%d", json_mgr_->get_int("battle", 1, "bega"));
-	ERROR("%s", json_mgr_->get_string("battle", 1, "roomName"));
+	ERROR("%d", json_mgr_->get_int("battle", 1, "began"));
+	ERROR("%s", json_mgr_->get_string("battle", 1, "roomName").c_str());
 	conn_mgr_->init();
 }
 

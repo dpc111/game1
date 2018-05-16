f7bef7da2e8615e6787489ad0e5284a2c912562d
dpc111 <1539309743@qq.com>
2018-05-16 18:30:56 +0800

    l

--- server/battle/battle/common/object_mgr_base.cpp
+++ server/battle/battle/common/object_mgr_base.cpp
@@ -1,6 +1,5 @@
 #include "object_mgr_base.h"
 #include "object_base.h"
-#include "id_generate.h"
 
 object_mgr_base_t::object_mgr_base_t() {
 	objects_.clear();
@@ -10,7 +9,7 @@ object_mgr_base_t::object_mgr_base_t() {
 
 object_mgr_base_t::~object_mgr_base_t() {
 	for (object_map_t::iterator it = objects_.begin(); it != objects_.end(); it++) {
-		object_t *object = it->second;
+		object_base_t *object = it->second;
 		delete object;
 	}
 	objects_.clear();
@@ -25,15 +24,15 @@ object_base_t *object_mgr_base_t::get_object(int32 object_id) {
 	return it->second;	
 }
 
-void object_mgr_base_t::add_object(int32 object_id, object_t *object) {
+void object_mgr_base_t::add_object(int32 object_id, object_base_t *object) {
 	if (get_object(object_id)) {
 		return;
 	}
 	objects_[object_id] = object;
-	object_num_++；
+	object_num_++;
 }
 
-void object_mgr_base_t::delete_object(int32 object_id) {
+void object_mgr_base_t::del_object(int32 object_id) {
 	object_map_t::iterator it = objects_.find(object_id);
 	if (it == objects_.end()) {
 		return;

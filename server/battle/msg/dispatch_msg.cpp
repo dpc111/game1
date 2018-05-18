6a90e9a0cc0b1a67d7cde7b51174d460c2f9a837
dpc111 <1539309743@qq.com>
2018-05-18 16:39:01 +0800

    l

--- server/battle/msg/dispatch_msg.cpp
+++ server/battle/msg/dispatch_msg.cpp
@@ -4,6 +4,13 @@
 #include "client_msg.h"
 #include "server_msg.h"
 
+#define REGISTER_DEFAULT_BIND(func, caller) \
+	std::tr1::bind(&func, \
+	caller, \
+	std::tr1::placeholders::_1, \
+	std::tr1::placeholders::_2, \
+	std::tr1::placeholders::_3)
+
 #define REGISTER_BIND(func, caller) \
 	std::tr1::bind(&func, \
 	caller, \
@@ -21,7 +28,7 @@ dispatch_msg_t::~dispatch_msg_t() {
 }
 
 void dispatch_msg_t::register_client_msg() {
-	service_->register_default_message(std::tr1::bind(&client_msg_t::default_message, client_msg_, std::tr1::placeholders::_1, std::tr1::placeholders::_2, std::tr1::placeholders::_3));	
+	service_->register_default_message(REGISTER_DEFAULT_BIND(client_msg_t::default_message, client_msg_));	
 	service_->register_net_message<battle_msg::s2c_join>(REGISTER_BIND(client_msg_t::s2c_join, client_msg_));	
 	service_->register_net_message<battle_msg::c2s_join>(REGISTER_BIND(client_msg_t::c2s_join, client_msg_));	
 	service_->register_net_message<battle_msg::c_login>(REGISTER_BIND(client_msg_t::c_login, client_msg_));	

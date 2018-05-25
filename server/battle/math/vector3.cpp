c8d64ad4fe159c087549e54c8113f3861d593b2c
dpc111 <1539309743@qq.com>
2018-05-25 20:18:09 +0800

    l

--- server/battle/math/vector3.cpp
+++ server/battle/math/vector3.cpp
@@ -1,13 +1,13 @@
-#include "vector3.h"
+// #include "vector3.h"
 
-vector3_t operator*(int s, const vector3_t& v) {
-    return v * (float)s;
-}
+// vector3_t operator*(int s, const vector3_t& v) {
+//     return v * (float)s;
+// }
 
-vector3_t operator*(float s, const vector3_t& v) {
-    return v * s;
-}
+// vector3_t operator*(float s, const vector3_t& v) {
+//     return v * s;
+// }
 
-vector3_t operator*(double s, const vector3_t& v) {
-    return v * (float)s;
-}
+// vector3_t operator*(double s, const vector3_t& v) {
+//     return v * (float)s;
+// }

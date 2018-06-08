849cab72b71bcf0106c7f1f5213a70169534e2a3
dpc111 <1539309743@qq.com>
2018-06-08 17:13:11 +0800

    l

--- server/common/common/log.h
+++ server/common/common/log.h
@@ -3,7 +3,8 @@
 
 #include "stdio.h"
 
-#define ERROR(fmt, ...) printf("\033[31m[%s][%d]", __FILE__, __LINE__); printf(fmt, ##__VA_ARGS__); printf("\n\033[0m")
+#define ERROR(fmt, ...) printf("\033[1;35m[%s][%d]", __FILE__, __LINE__); printf(fmt, ##__VA_ARGS__); printf("\n\033[0m")
+#define WARN(fmt, ...) printf("\033[33m[%s][%d]", __FILE__, __LINE__); printf(fmt, ##__VA_ARGS__); printf("\n\033[0m")
 #define LOG(fmt, ...) printf("\033[37m[%s][%d]", __FILE__, __LINE__); printf(fmt, ##__VA_ARGS__); printf("\n\033[0m")
 
 #endif

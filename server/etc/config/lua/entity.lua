48671f8adbdb4c301f49f31229bb06aaeabd1301
dpc111 <1539309743@qq.com>
2018-06-09 18:58:46 +0800

    l

--- server/etc/config/lua/entity.lua
+++ server/etc/config/lua/entity.lua
@@ -1,6 +1,6 @@
 cfg_entity = {
-[1] = {id=1,open=1,cd=3,cost=500,blood=5000,bullet=1,damage=100,frozen=0,box={2,3},gun_pos={1,2},fire_begin_time=2,fire_end_time=1,entity_prefab=[[Entity/Entity1]],},
-[2] = {id=2,open=0,cd=1,cost=4000,blood=40000,bullet=2,damage=600,frozen=0,box={2,3},gun_pos={1,2},fire_begin_time=1,fire_end_time=1,entity_prefab=[[Entity/Entity2]],},
-[3] = {id=3,open=1,cd=3,cost=10000,blood=100000,bullet=3,damage=1000,frozen=0.5,box={2,3},gun_pos={1,2},fire_begin_time=1,fire_end_time=1,entity_prefab=[[Entity/Entity3]],},
-[4] = {id=4,open=1,cd=2,cost=20000,blood=200000,bullet=4,damage=1500,frozen=0,box={2,3},gun_pos={1,2},fire_begin_time=2,fire_end_time=1,entity_prefab=[[Entity/Entity3]],},
+[1] = {id=1,open=1,cd=3,cost=500,blood=5000,bullet=1,damage=100,frozen=0,box={2,0,3},gun_pos={1,2,0},fire_begin_time=2,fire_end_time=1,entity_prefab=[[Entity/Entity1]],},
+[2] = {id=2,open=0,cd=1,cost=4000,blood=40000,bullet=2,damage=600,frozen=0,box={2,0,3},gun_pos={1,2,0},fire_begin_time=1,fire_end_time=1,entity_prefab=[[Entity/Entity2]],},
+[3] = {id=3,open=1,cd=3,cost=10000,blood=100000,bullet=3,damage=1000,frozen=0.5,box={2,0,3},gun_pos={1,2,0},fire_begin_time=1,fire_end_time=1,entity_prefab=[[Entity/Entity3]],},
+[4] = {id=4,open=1,cd=2,cost=20000,blood=200000,bullet=4,damage=1500,frozen=0,box={2,0,3},gun_pos={1,2,0},fire_begin_time=2,fire_end_time=1,entity_prefab=[[Entity/Entity3]],},
 }

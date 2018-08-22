afe09e85416668aef81e08c7938facb177426a15
dpc111 <1539309743@qq.com>
2018-08-22 11:35:54 +0800

    l

--- server/common/net/rudp/rudp.h
+++ server/common/net/rudp/rudp.h
@@ -1,8 +1,6 @@
 #ifndef UDP_RUDP_H
 #define UDP_RUDP_H
 
-#define UDP_DATA_MAX_LEN 512		// Internet(非局域网)上的标准MTU值为576字节 (控制在548字节 (576-8-20)以内)
-
 #define UDP_HEAD_BYTE_LEN 1
 #define UDP_HEAD_BYTE_TYPE 1
 #define UDP_HEAD_BYTE_SEQ 4
@@ -17,6 +15,10 @@
 #define UDP_REQ_AGAIN_TICKS 10
 #define UDP_REQ_AGAIN_MAX_TIMES 10
 #define UDP_RECV_SEQ_MAX_DEV 20
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void* revc_buff_in(struct udp_handle_t *h);
 
@@ -35,5 +37,9 @@ struct udp_chunk_t * send_buff_out(struct udp_handle_t *h)
 void send_buff_out_process(struct udp_handle_t *h);
 
 void udp_process(struct udp_handle_t *h, int64 tick);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- server/common/net/rudp/udp_pool.h
+++ server/common/net/rudp/udp_pool.h
@@ -11,8 +11,16 @@ struct udp_chunk_pool_t {
 	int size;
 };
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct udp_chunk_t* chunk_pool_malloc(struct udp_chunk_pool_t *pool);
 
 void chunk_pool_free(struct udp_chunk_pool_t *pool, struct udp_chunk_t *chunk)
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif

0045f3d1ff516dd89fcddc863a6997f81498b809
dpc111 <1539309743@qq.com>
2018-08-23 16:19:27 +0800

    l

--- server/common/net/net_common.h
+++ server/common/net/net_common.h
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
 
 #define SET_SOCKET_NONBLOCK(fd) \
 int flags = fcntl(fd, F_GETFL, 0); \

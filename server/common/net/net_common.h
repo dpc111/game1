7f664c47733b3c5a575234a95431011b1a37a80c
dpc111 <1539309743@qq.com>
2018-08-23 16:12:45 +0800

    l

--- server/common/net/net_common.h
+++ server/common/net/net_common.h
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 #define SET_SOCKET_NONBLOCK(fd) \
 int flags = fcntl(fd, F_GETFL, 0); \
 fcntl(fd, F_SETFL, flags | O_NONBLOCK); \

ec4dd13c0dd87022b410500cfcf2b8cb9aaa778e
dpc111 <1539309743@qq.com>
2018-11-19 15:24:49 +0800

    l

--- server/common/net/rudp/udp_chunk.h
+++ server/common/net/rudp/udp_chunk.h
@@ -10,19 +10,27 @@
     typedef     __int16             int16;
     typedef     __int32             int32;
     typedef     __int64             int64;
+    typedef     unsigned __int8     uint8;
+    typedef     unsigned __int16    uint16;
+    typedef     unsigned __int32    uint32;
+    typedef     unsigned __int64    uint64;
 #else
     typedef     char                int8;
     typedef     short               int16;
     typedef     int                 int32;
     typedef     long long           int64;
+    typedef     unsigned char       uint8;
+    typedef     unsigned short      uint16;
+    typedef     unsigned int        uint32;
+    typedef     unsigned long long  uint64;
 #endif
 
 struct udp_chunk_t;
 
 #pragma pack(1)
 typedef struct udp_chunk_t {
-	int8 size;									// size of buff
-	int8 type;
+	uint8 size;									// size of buff
+	uint8 type;
 	int32 seq;
 	int32 ack;
 	char buff[UDP_DATA_MAX_LEN];
